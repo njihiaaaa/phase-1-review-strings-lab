a7b111e40684a79fb23f39218e2b6d8b69a0f315
Michael Njihia <michael.njihia@student.moringaschool.com>
2024-09-26 20:51:48 +0300

    js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+const currentUser ='Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const upperCaseCurrentUser = currentUser.toUpperCase()
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${firstInitial}!`;
